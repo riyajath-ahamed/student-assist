e1b968c48057642fb0a5be39299af292de048b30
riyajath-ahamed <riyajatha@gmail.com>
2021-09-30 01:51:11 +0530

    poddak error heduwa ><

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,33 +1,70 @@
 import React, {useContext} from "react";
-import {View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Image, } from 'react-native';
 
-import{Container} from '/Programming/Student/styles/FeedStyles'
+//import{Card, Container} from '/Programming/Student/styles/FeedStyles'
+
 
 import { AuthContext } from "../navigation/AuthProvider";
+
+import PostCard from "../asset/components/PostCard";
+import { Card, Container,  UserInfo, UserImg, UserName, UserInfoText, PostTime, PostText, PostImg, InteractionWrapper, Interaction, Divider, InteractionText } from "../styles/FeedStyles";
 
 const HomeScreen =() => {
     const {user, logout}= useContext(AuthContext);
 
     return(
         <Container>
-            <Text styles={styles.text}> Welcome </Text>
+           
+            <Card>
+                <UserInfo>
+                    <UserImg source={require('../asset/user/user1.jpeg')} /> 
+                    <UserInfoText>
+                    <UserName>Dilshan</UserName>
+                    <PostTime>4 Hours ago</PostTime>
+                    </UserInfoText>
+                </UserInfo>
+                <PostText>
+                    There will be a Meeting In our Auditorim
+                </PostText>
+                <PostImg source={require('../asset/Post/post1.jpg')} />
+                <InteractionWrapper>
+                    <Interaction active >
+                        <Image source={require('../asset/Icon/thumbs.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
+                        <InteractionText active > 12 Noted</InteractionText>
+                    </Interaction>
+                    <Interaction>
+                        <Image source={require('../asset/Icon/comment.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
+                        <Text> Comment</Text>
+                    </Interaction>
+                </InteractionWrapper>
+            </Card>
+
+            <Card>
+                <UserInfo>
+                    <UserImg source={require('../asset/user/user1.jpeg')} /> 
+                    <UserInfoText>
+                    <UserName>Dilshan</UserName>
+                    <PostTime>4 Hours ago</PostTime>
+                    </UserInfoText>
+                </UserInfo>
+                <PostText>
+                    This is Your new Assitant "Student Assit" !!!
+                </PostText>
+                <Divider/>
+                <InteractionWrapper>
+                    <Interaction>
+                        <Image source={require('../asset/Icon/thumbs.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
+                        <Text> Noted</Text>
+                    </Interaction>
+                    <Interaction>
+                        <Image source={require('../asset/Icon/comment.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
+                        <Text> Comment</Text>
+                    </Interaction>
+                </InteractionWrapper>
+            </Card>
+            
         </Container>
     );
 }
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-    container:{
-        backgroundColor: '#f9fafd',
-        flex: 1,
-        alignItems:'center',
-        justifyContent:'center',
-        padding: 20,
-    },
-    text: {
-        fontSize:20,
-        color: '#333333',
-        fontWeight: 'bold'
-    }
-})
