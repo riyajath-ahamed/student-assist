291bf2d3b2ff8ddcd545145a1c62d97fe6790907
riyajath-ahamed <riyajatha@gmail.com>
2021-09-30 18:35:20 +0530

    change12

--- asset/components/PostCard.js
+++ asset/components/PostCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { Card, Container,  UserInfo, UserImg, UserName, UserInfoText, PostTime, PostText, PostImg, InteractionWrapper, Interaction, Divider, InteractionText } from "../styles/FeedStyles";
+import { Card, Container,  UserInfo, UserImg, UserName, UserInfoText, PostTime, PostText, PostImg, InteractionWrapper, Interaction, Divider, InteractionText } from "../../styles/FeedStyles";
 
 const PostCard = () => {
 
     return(
         <Card>
                 <UserInfo>
-                    <UserImg source={require('../asset/user/user1.jpeg')} /> 
+                    <UserImg source={require('../../asset/user/user1.jpeg')} /> 
                     <UserInfoText>
                     <UserName>Dilshan</UserName>
                     <PostTime>4 Hours ago</PostTime>
@@ -16,14 +16,14 @@ const PostCard = () => {
                 <PostText>
                     There will be a Meeting In our Auditorim
                 </PostText>
-                <PostImg source={require('../asset/Post/post1.jpg')} />
+                <PostImg source={require('../../asset/Post/post1.jpg')} />
                 <InteractionWrapper>
                     <Interaction active >
-                        <Image source={require('../asset/Icon/thumbs.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
+                        <Image source={require('../../asset/Icon/thumbs.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
                         <InteractionText active > 12 Noted</InteractionText>
                     </Interaction>
                     <Interaction>
-                        <Image source={require('../asset/Icon/comment.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
+                        <Image source={require('../../asset/Icon/comment.png')} resizeMode="contain" style={{ width: 20, height: 20,}}/>
                         <Text> Comment</Text>
                     </Interaction>
                 </InteractionWrapper>
--- styles/FeedStyles.js
+++ styles/FeedStyles.js
@@ -13,3 +13,72 @@ export const Card = styled.View`
     margin-bottom: 20px;
     border-radius: 10px;
 `;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    justify-content: flex-start;
+`;
+
+export const UserImg = styled.Image`
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+`;
+export const UserInfoText = styled.View`
+    flex-direction: column;
+    justify-content: center;
+    padding: 10px;     
+`;
+
+export const UserName = styled.Text`
+    font-size: 14px;
+    font-weight: bold;
+    font-family: 'Lato-Regular'
+`;
+
+export const PostTime = styled.Text`
+    font-size: 12px;
+    font-family: 'Lato-Regular';
+    color: #666;
+`; 
+export const PostText = styled.Text`
+    font-size: 14px;
+    font-family: 'Lato-Regular';
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-bottom: 15px;
+`;
+export const Divider = styled.View`
+    border-bottom-color: #dddddd;
+    border-bottom-width: 1px;
+    width: 92%;
+    align-self: center;
+    margin-top: 15px;
+`;
+
+export const PostImg = styled.Image`
+    width: 100%;
+    height: 250px;
+`;
+
+export const InteractionWrapper = styled.View`
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 15px;
+`;
+
+export const Interaction = styled.TouchableOpacity`
+    flex-direction: row;
+    justify-content: center;
+    border-radius: 5px;
+    padding: 2px 5px;
+    background-color: ${props => props.active ? '#2e64e515' : 'transparent'}
+`;
+export const InteractionText = styled.Text`
+    font-size: 12px;
+    font-family: 'Lato-Regular';
+    font-weight: bold;
+    color: ${props => props.active ? '#2e64e5' : '#333'};
+    margin-top: 5px;
+    margin-left: 5px;
+`;
